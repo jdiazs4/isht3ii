7db664caa429ac4cd2d9e55965f03b8493d82e51
JOAN MANUEL DIAZ SALAZAR <jdiazs4@miumg.edu.gt>
2019-06-02 23:18:26 -0600

    Creacion de ejemplos en funcion main

--- isht3ii/src/main/java/com/mycompany/mavenproject1/Palindromes.java
+++ isht3ii/src/main/java/com/mycompany/mavenproject1/Palindromes.java
@@ -11,7 +11,58 @@ package com.mycompany.mavenproject1;
  */
 public class Palindromes {
     public static void main(String args[]){
+        String word = null;
+        boolean resultado= false;
         
+        //EJEMPLO 1 - PALABRAS
+        word = "malayalam";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 2 - PALABRAS
+        word = "sometemos";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 3 - PALABRAS
+        word = "reconocer";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 4 - FRASES
+        word = "ali tomo tila";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 5 - FRASES
+        word = "amo la pacifica paloma";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 6 - FRASES
+        word = "eva usaba rimel y le miraba suave";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 7 - NUMEROS
+        word = "1234567890987654321";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 8 - NUMEROS
+        word = "2002";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 9 - CARACTERES ESPECIALES 
+        word = "?/.,*-+o+-*,./?";
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
+        
+        //EJEMPLO 10 - NULO
+        word = null;
+        resultado = evaluate(word);
+        System.out.println("Probando: " + word + " - Resultado: " + resultado);
     }
     
     public static boolean evaluate(String word) {
